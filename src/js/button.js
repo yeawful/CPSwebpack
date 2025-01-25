0d30529c2215447eeac27b28375a65e5afd422ff
yeawful <paketa1337@gmail.com>
2025-01-26 01:13:49 +0300

    update

--- src/js/button.js
+++ src/js/button.js
@@ -1,4 +1,14 @@
-// Функция для переключения состояния секции (раскрытие/скрытие)
+// ==================== UTILITY FUNCTIONS ====================
+
+/**
+ * Переключает состояние секции (раскрытие/скрытие)
+ * @param {HTMLElement} section - Секция, которую нужно раскрыть/скрыть
+ * @param {HTMLElement} button - Кнопка, которая управляет состоянием секции
+ * @param {HTMLElement} buttonText - Элемент, содержащий текст кнопки
+ * @param {string} expandedClass - Класс, который добавляется/удаляется при раскрытии
+ * @param {string} showText - Текст кнопки в состоянии "показать"
+ * @param {string} hideText - Текст кнопки в состоянии "скрыть"
+ */
 function toggleSection(
   section,
   button,
@@ -13,11 +23,15 @@ function toggleSection(
     : showText
 }
 
-// Функция для закрытия элемента при клике вне его области
-// eslint-disable-next-line no-unused-vars
+/**
+ * Закрывает элемент при клике вне его области
+ * @param {HTMLElement} element - Элемент, который нужно закрыть
+ * @param {NodeList} buttons - Кнопки, которые не должны закрывать элемент
+ * @param {Function} closeFunction - Функция, которая выполняет закрытие
+ */
 function closeOnClickOutside(element, buttons, closeFunction) {
   document.addEventListener('click', function (event) {
-    const buttonsArray = Array.from(buttons) // Преобразуем NodeList в массив
+    const buttonsArray = Array.from(buttons)
     if (
       !element.contains(event.target) &&
       !buttonsArray.some((button) => button.contains(event.target))
@@ -27,51 +41,77 @@ function closeOnClickOutside(element, buttons, closeFunction) {
   })
 }
 
-// Функция для настройки модального окна
+/**
+ * Настраивает модальное окно
+ * @param {HTMLElement} modal - Модальное окно
+ * @param {NodeList} buttons - Кнопки, открывающие модальное окно
+ * @param {HTMLElement} closeButton - Кнопка закрытия модального окна
+ */
 function setupModal(modal, buttons, closeButton) {
-  if (modal && buttons.length > 0 && closeButton) {
-    buttons.forEach((button) => {
-      button.addEventListener('click', function () {
-        modal.classList.add('modal--shown')
-        overlay.classList.add('overlay--visible')
-        pageContainer.classList.add('blur-background')
+  if (!modal || buttons.length === 0 || !closeButton) return
 
-        // Перемещаем фокус на первый фокусируемый элемент модального окна
-        const firstFocusableElement = modal.querySelector(
-          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-        )
-        if (firstFocusableElement) {
-          firstFocusableElement.focus()
-        }
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      modal.classList.add('modal--shown')
+      overlay.classList.add('overlay--visible')
+      pageContainer.classList.add('blur-background')
 
-        // Закрываем меню, если оно открыто
-        if (menu.classList.contains('menu--shown')) {
-          menu.classList.remove('menu--shown')
-        }
-      })
+      const firstFocusableElement = modal.querySelector(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+      )
+      if (firstFocusableElement) firstFocusableElement.focus()
+
+      if (menu.classList.contains('menu--shown')) {
+        menu.classList.remove('menu--shown')
+      }
     })
+  })
 
-    closeButton.addEventListener('click', function () {
-      modal.classList.remove('modal--shown')
-      overlay.classList.remove('overlay--visible')
-      pageContainer.classList.remove('blur-background')
+  closeButton.addEventListener('click', () => {
+    modal.classList.remove('modal--shown')
+    overlay.classList.remove('overlay--visible')
+    pageContainer.classList.remove('blur-background')
+    buttons[0].focus()
+  })
 
-      // Возвращаем фокус на кнопку, которая открыла модальное окно
-      buttons[0].focus()
-    })
-
-    overlay.addEventListener('click', function () {
-      modal.classList.remove('modal--shown')
-      overlay.classList.remove('overlay--visible')
-      pageContainer.classList.remove('blur-background')
-
-      // Возвращаем фокус на кнопку, которая открыла модальное окно
-      buttons[0].focus()
-    })
-  }
+  overlay.addEventListener('click', () => {
+    modal.classList.remove('modal--shown')
+    overlay.classList.remove('overlay--visible')
+    pageContainer.classList.remove('blur-background')
+    buttons[0].focus()
+  })
 }
 
-// Управление текстом "Читать далее"
+/**
+ * Ловушка фокуса для модальных окон
+ * @param {HTMLElement} modal - Модальное окно
+ */
+function trapFocus(modal) {
+  const focusableElements = modal.querySelectorAll(
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+  )
+  const firstFocusableElement = focusableElements[0]
+  const lastFocusableElement = focusableElements[focusableElements.length - 1]
+
+  modal.addEventListener('keydown', function (event) {
+    if (event.key === 'Tab') {
+      if (event.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus()
+          event.preventDefault()
+        }
+      } else {
+        if (document.activeElement === lastFocusableElement) {
+          firstFocusableElement.focus()
+          event.preventDefault()
+        }
+      }
+    }
+  })
+}
+
+// ==================== DOM ELEMENTS ====================
+
 const aboutText = document.querySelector('.about__text')
 const readMoreButton = document.querySelector('.about__read-more')
 const readMoreButtonText = document.querySelector(
@@ -79,8 +119,33 @@ const readMoreButtonText = document.querySelector(
 )
 const hiddenParagraph = document.querySelector('.about__paragraph--hidden')
 
+const list = document.querySelector('.brands__list')
+const expandButton = document.querySelector('.brands__expand-btn')
+const expandButtonText = document.querySelector('.brands__expand-text')
+
+const sectionList = document.querySelector('.devices__list')
+const sectionExpandButton = document.querySelector('.devices__expand-btn')
+const sectionExpandButtonText = document.querySelector('.devices__expand-text')
+
+const burgerMenuButton = document.querySelector('.nav__burger')
+const menu = document.querySelector('.menu')
+const closeMenuButton = document.querySelector('.menu__btn--close')
+const overlay = document.querySelector('.overlay')
+const pageContainer = document.querySelector('.page-container')
+
+const callModal = document.querySelector('.modal.call')
+const callModalButtons = document.querySelectorAll('.btn--call')
+const closeCallModalButton = callModal?.querySelector('.modal__btn')
+
+const feedbackModal = document.querySelector('.modal.feedback')
+const feedbackModalButtons = document.querySelectorAll('.btn--chat')
+const closeFeedbackModalButton = feedbackModal?.querySelector('.modal__btn')
+
+// ==================== EVENT LISTENERS ====================
+
+// Управление текстом "Читать далее"
 if (aboutText && readMoreButton && readMoreButtonText && hiddenParagraph) {
-  readMoreButton.addEventListener('click', function () {
+  readMoreButton.addEventListener('click', () => {
     aboutText.classList.toggle('about__text--expanded')
     hiddenParagraph.classList.toggle('about__paragraph--hidden')
     readMoreButtonText.textContent = aboutText.classList.contains(
@@ -92,12 +157,8 @@ if (aboutText && readMoreButton && readMoreButtonText && hiddenParagraph) {
 }
 
 // Управление списком брендов
-const list = document.querySelector('.brands__list')
-const expandButton = document.querySelector('.brands__expand-btn')
-const expandButtonText = document.querySelector('.brands__expand-text')
-
 if (list && expandButton && expandButtonText) {
-  expandButton.addEventListener('click', function () {
+  expandButton.addEventListener('click', () => {
     toggleSection(
       list,
       expandButton,
@@ -110,12 +171,8 @@ if (list && expandButton && expandButtonText) {
 }
 
 // Управление списком устройств
-const sectionList = document.querySelector('.devices__list')
-const sectionExpandButton = document.querySelector('.devices__expand-btn')
-const sectionExpandButtonText = document.querySelector('.devices__expand-text')
-
 if (sectionList && sectionExpandButton && sectionExpandButtonText) {
-  sectionExpandButton.addEventListener('click', function () {
+  sectionExpandButton.addEventListener('click', () => {
     toggleSection(
       sectionList,
       sectionExpandButton,
@@ -128,115 +185,60 @@ if (sectionList && sectionExpandButton && sectionExpandButtonText) {
 }
 
 // Управление бургер-меню
-const burgerMenuButton = document.querySelector('.nav__burger')
-const menu = document.querySelector('.menu')
-const closeMenuButton = document.querySelector('.menu__btn--close')
-const overlay = document.querySelector('.overlay')
-const pageContainer = document.querySelector('.page-container')
-
 if (burgerMenuButton && menu && closeMenuButton && overlay && pageContainer) {
-  burgerMenuButton.addEventListener('click', function () {
+  burgerMenuButton.addEventListener('click', () => {
     menu.classList.add('menu--shown')
     overlay.classList.add('overlay--visible')
     pageContainer.classList.add('blur-background')
 
-    // Перемещаем фокус на первый элемент меню
     const firstFocusableElement = menu.querySelector(
       'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
     )
-    if (firstFocusableElement) {
-      firstFocusableElement.focus()
-    }
+    if (firstFocusableElement) firstFocusableElement.focus()
   })
 
-  closeMenuButton.addEventListener('click', function () {
+  closeMenuButton.addEventListener('click', () => {
     menu.classList.remove('menu--shown')
     overlay.classList.remove('overlay--visible')
     pageContainer.classList.remove('blur-background')
-
-    // Возвращаем фокус на кнопку бургер-меню
     burgerMenuButton.focus()
   })
 
-  overlay.addEventListener('click', function () {
+  overlay.addEventListener('click', () => {
     menu.classList.remove('menu--shown')
     overlay.classList.remove('overlay--visible')
     pageContainer.classList.remove('blur-background')
-
-    // Возвращаем фокус на кнопку бургер-меню
     burgerMenuButton.focus()
   })
 }
 
 // Управление модальными окнами
-const callModal = document.querySelector('.modal.call')
-const callModalButtons = document.querySelectorAll('.btn--call')
-const closeCallModalButton = callModal?.querySelector('.modal__btn')
-
-const feedbackModal = document.querySelector('.modal.feedback')
-const feedbackModalButtons = document.querySelectorAll('.btn--chat')
-const closeFeedbackModalButton = feedbackModal?.querySelector('.modal__btn')
-
 setupModal(callModal, callModalButtons, closeCallModalButton)
 setupModal(feedbackModal, feedbackModalButtons, closeFeedbackModalButton)
 
 // Закрытие модальных окон и меню по нажатию на Esc
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
-    // Закрываем модальные окна
     const openModal = document.querySelector('.modal--shown')
     if (openModal) {
       openModal.classList.remove('modal--shown')
       overlay.classList.remove('overlay--visible')
       pageContainer.classList.remove('blur-background')
 
-      // Возвращаем фокус на кнопку, которая открыла модальное окно
       const modalButtons = openModal.classList.contains('call')
         ? callModalButtons
         : feedbackModalButtons
-      if (modalButtons.length > 0) {
-        modalButtons[0].focus()
-      }
+      if (modalButtons.length > 0) modalButtons[0].focus()
     }
 
-    // Закрываем меню
     if (menu.classList.contains('menu--shown')) {
       menu.classList.remove('menu--shown')
       overlay.classList.remove('overlay--visible')
       pageContainer.classList.remove('blur-background')
-
-      // Возвращаем фокус на кнопку бургер-меню
       burgerMenuButton.focus()
     }
   }
 })
-
-// Ловушка фокуса для модальных окон
-function trapFocus(modal) {
-  const focusableElements = modal.querySelectorAll(
-    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-  )
-  const firstFocusableElement = focusableElements[0]
-  const lastFocusableElement = focusableElements[focusableElements.length - 1]
-
-  modal.addEventListener('keydown', function (event) {
-    if (event.key === 'Tab') {
-      if (event.shiftKey) {
-        // Shift + Tab
-        if (document.activeElement === firstFocusableElement) {
-          lastFocusableElement.focus()
-          event.preventDefault()
-        }
-      } else {
-        // Tab
-        if (document.activeElement === lastFocusableElement) {
-          firstFocusableElement.focus()
-          event.preventDefault()
-        }
-      }
-    }
-  })
-}
 
 // Применяем ловушку фокуса для всех модальных окон
 if (callModal) trapFocus(callModal)
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,96 +2,112 @@
 const mediaQuery = window.matchMedia('(max-width: 767px)')
 const swiperInstances = {}
 
-function initSwiper(blockClass) {
-  if (mediaQuery.matches && !swiperInstances[blockClass]) {
-    const swiperContainer = document.querySelector(`.${blockClass}__content`)
-    const swiperWrapper = document.querySelector(`.${blockClass}__list`)
-    const swiperSlides = document.querySelectorAll(`.${blockClass}__item`)
-    const swiperPagination = document.querySelector(
-      `.${blockClass} .swiper-pagination`
-    )
+function initializeSwiperElements(blockClass) {
+  const swiperContainer = document.querySelector(`.${blockClass}__content`)
+  const swiperWrapper = document.querySelector(`.${blockClass}__list`)
+  const swiperSlides = document.querySelectorAll(`.${blockClass}__item`)
+  const swiperPagination = document.querySelector(
+    `.${blockClass} .swiper-pagination`
+  )
 
-    if (!swiperContainer || !swiperWrapper || swiperSlides.length === 0) {
-      return
-    }
+  if (!swiperContainer || !swiperWrapper || swiperSlides.length === 0) {
+    return null
+  }
 
-    swiperContainer.classList.add('swiper')
-    swiperWrapper.classList.add('swiper-wrapper')
-    swiperSlides.forEach((slide) => {
-      slide.classList.add('swiper-slide')
-      slide.setAttribute('tabindex', '0') // Добавляем tabindex к каждому слайду
-    })
+  swiperContainer.classList.add('swiper')
+  swiperWrapper.classList.add('swiper-wrapper')
+  swiperSlides.forEach((slide) => {
+    slide.classList.add('swiper-slide')
+    slide.setAttribute('tabindex', '0') // Добавляем tabindex к каждому слайду
+  })
 
-    // Добавляем ARIA-атрибуты
-    swiperContainer.setAttribute('role', 'region')
-    swiperContainer.setAttribute('aria-label', 'Слайдер')
-    swiperWrapper.setAttribute('role', 'list')
-    swiperSlides.forEach((slide, index) => {
-      slide.setAttribute('role', 'listitem')
-      slide.setAttribute('aria-label', `Слайд ${index + 1}`)
-    })
+  // Добавляем ARIA-атрибуты
+  swiperContainer.setAttribute('role', 'region')
+  swiperContainer.setAttribute('aria-label', 'Слайдер')
+  swiperWrapper.setAttribute('role', 'list')
+  swiperSlides.forEach((slide, index) => {
+    slide.setAttribute('role', 'listitem')
+    slide.setAttribute('aria-label', `Слайд ${index + 1}`)
+  })
 
-    // eslint-disable-next-line no-undef
-    swiperInstances[blockClass] = new Swiper(`.${blockClass}__content`, {
-      pagination: swiperPagination
-        ? {
-            el: swiperPagination,
-            type: 'bullets',
-            clickable: true
-          }
-        : false,
-      simulateTouch: true,
-      grabCursor: true,
-      slideToClickedSlide: true,
-      spaceBetween: 16,
-      slidesOffsetAfter: 32,
-      slidesPerView: 1.2,
-      breakpoints: {
-        320: { slidesPerView: 1.2 },
-        480: { slidesPerView: 1.6 },
-        560: { slidesPerView: 1.8 },
-        640: { slidesPerView: 2.0 },
-        720: { slidesPerView: 2.2 }
-      },
-      on: {
-        slideChange: function () {
-          const activeSlide = this.slides[this.activeIndex]
-          if (activeSlide) {
-            activeSlide.focus() // Перемещаем фокус на активный слайд
-          }
+  return { swiperContainer, swiperPagination }
+}
+
+function configureSwiper(blockClass, swiperContainer, swiperPagination) {
+  // eslint-disable-next-line no-undef
+  swiperInstances[blockClass] = new Swiper(swiperContainer, {
+    pagination: swiperPagination
+      ? {
+          el: swiperPagination,
+          type: 'bullets',
+          clickable: true
+        }
+      : false,
+    simulateTouch: true,
+    grabCursor: true,
+    slideToClickedSlide: true,
+    spaceBetween: 16,
+    slidesOffsetAfter: 32,
+    slidesPerView: 1.2,
+    breakpoints: {
+      320: { slidesPerView: 1.2 },
+      480: { slidesPerView: 1.6 },
+      560: { slidesPerView: 1.8 },
+      640: { slidesPerView: 2.0 },
+      720: { slidesPerView: 2.2 }
+    },
+    on: {
+      slideChange: function () {
+        const activeSlide = this.slides[this.activeIndex]
+        if (activeSlide) {
+          activeSlide.focus() // Перемещаем фокус на активный слайд
         }
       }
-    })
+    }
+  })
 
-    if (swiperPagination) {
-      swiperPagination.style.display = mediaQuery.matches ? 'block' : 'none'
-      const paginationBullets = swiperPagination.querySelectorAll(
-        '.swiper-pagination-bullet'
-      )
-      paginationBullets.forEach((bullet) => {
-        bullet.setAttribute('tabindex', '0') // Делаем элементы пагинации фокусируемыми
-      })
+  if (swiperPagination) {
+    swiperPagination.style.display = mediaQuery.matches ? 'block' : 'none'
+    const paginationBullets = swiperPagination.querySelectorAll(
+      '.swiper-pagination-bullet'
+    )
+    paginationBullets.forEach((bullet) => {
+      bullet.setAttribute('tabindex', '0') // Делаем элементы пагинации фокусируемыми
+    })
+  }
+}
+
+function destroySwiper(blockClass) {
+  if (swiperInstances[blockClass] && swiperInstances[blockClass].destroy) {
+    swiperInstances[blockClass].destroy(true, true)
+  }
+  swiperInstances[blockClass] = null
+
+  const swiperContainer = document.querySelector(`.${blockClass}__content`)
+  const swiperWrapper = document.querySelector(`.${blockClass}__list`)
+  const swiperSlides = document.querySelectorAll(`.${blockClass}__item`)
+
+  if (swiperContainer) swiperContainer.classList.remove('swiper')
+  if (swiperWrapper) swiperWrapper.classList.remove('swiper-wrapper')
+  swiperSlides.forEach((slide) => slide.classList.remove('swiper-slide'))
+
+  const swiperPagination = document.querySelector(
+    `.${blockClass} .swiper-pagination`
+  )
+  if (swiperPagination) {
+    swiperPagination.style.display = 'none'
+  }
+}
+
+function initSwiper(blockClass) {
+  if (mediaQuery.matches && !swiperInstances[blockClass]) {
+    const swiperElements = initializeSwiperElements(blockClass)
+    if (swiperElements) {
+      const { swiperContainer, swiperPagination } = swiperElements
+      configureSwiper(blockClass, swiperContainer, swiperPagination)
     }
   } else if (!mediaQuery.matches && swiperInstances[blockClass]) {
-    if (swiperInstances[blockClass] && swiperInstances[blockClass].destroy) {
-      swiperInstances[blockClass].destroy(true, true)
-    }
-    swiperInstances[blockClass] = null
-
-    const swiperContainer = document.querySelector(`.${blockClass}__content`)
-    const swiperWrapper = document.querySelector(`.${blockClass}__list`)
-    const swiperSlides = document.querySelectorAll(`.${blockClass}__item`)
-
-    if (swiperContainer) swiperContainer.classList.remove('swiper')
-    if (swiperWrapper) swiperWrapper.classList.remove('swiper-wrapper')
-    swiperSlides.forEach((slide) => slide.classList.remove('swiper-slide'))
-
-    const swiperPagination = document.querySelector(
-      `.${blockClass} .swiper-pagination`
-    )
-    if (swiperPagination) {
-      swiperPagination.style.display = 'none'
-    }
+    destroySwiper(blockClass)
   }
 }
 
