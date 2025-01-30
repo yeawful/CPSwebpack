4d0759c567204a372ec3e5d860310d4b202a0d9e
yeawful <paketa1337@gmail.com>
2025-01-30 09:04:05 +0300

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path')
+const path = require('path') // Подключаем модуль path для работы с путями файловой системы
 
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin') // Подключаем плагин для копирования файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin') // Подключаем плагин для работы с HTML
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // Подключаем плагин для извлечения CSS в отдельный файл
 
 module.exports = {
-  entry: ['./src/js/index.js'],
+  entry: ['./src/js/index.js'], // Указываем точку входа - главный JS файл
   output: {
-    filename: './js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: './js/bundle.js', // Имя выходного JS файла
+    path: path.resolve(__dirname, 'dist'), // Путь для выходных файлов
+    clean: true // Очищать папку dist перед каждой сборкой
   },
-  devtool: 'source-map',
+  devtool: 'source-map', // Генерировать source maps для отладки
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        exclude: /node_modules/,
+        test: /\.js$/, // Обрабатывать файлы с расширением .js
+        include: path.resolve(__dirname, 'src/js'), // Обрабатывать только файлы из папки src/js
+        exclude: /node_modules/, // Исключить папку node_modules
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // Использовать babel-loader для транспиляции JS
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'] // Использовать preset-env для поддержки современных стандартов JS
           }
         }
       },
       {
-        test: /\.scss$/,
+        test: /\.scss$/, // Обрабатывать файлы с расширением .scss
         use: [
           MiniCssExtractPlugin.loader, // Извлекает CSS в отдельный файл
           'css-loader', // Переводит CSS в CommonJS
@@ -35,39 +35,39 @@ module.exports = {
         ]
       },
       {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        type: 'asset/resource',
+        test: /\.(eot|ttf|woff|woff2)$/, // Обрабатывать шрифты
+        type: 'asset/resource', // Использовать asset/resource для обработки файлов
         generator: {
-          filename: 'fonts/[name][ext]'
+          filename: 'fonts/[name][ext]' // Сохранять шрифты в папку fonts
         }
       },
       {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
-        type: 'asset/resource',
+        test: /\.(svg|png|jpg|jpeg|webp)$/, // Обрабатывать изображения
+        type: 'asset/resource', // Использовать asset/resource для обработки файлов
         generator: {
-          filename: 'img/[name][ext]'
+          filename: 'img/[name][ext]' // Сохранять изображения в папку img
         }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'CPS Full',
-      template: './src/index.html',
-      inject: true,
+      title: 'CPS Full', // Заголовок HTML файла
+      template: './src/index.html', // Шаблон HTML файла
+      inject: true, // Внедрять скрипты и стили в HTML
       minify: {
-        removeComments: true,
-        collapseWhitespace: false
+        removeComments: true, // Удалять комментарии в HTML
+        collapseWhitespace: false // Не удалять пробелы
       }
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: 'style.css' // Имя выходного CSS файла
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: './src/img',
-          to: 'img'
+          from: './src/img', // Копировать файлы из папки src/img
+          to: 'img' // В папку dist/img
         }
       ]
     })
